6b2b19ed338de5fa4b70572aeb14bb204c116e82
Ivan Andrade <ivandandrade90@gmail.com>
2021-04-23 23:15:59 -0300

    First commit, Hello World! class

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+* HelloWorld
+*/
+public class HelloWorld {
+
+	
+  public static void main(String[] args) {
+  	
+    String greeting = "Hello, World!";
+    System.out.println(greeting);
+  }
+}
---
+++ Readme.md
@@ -0,0 +1,1 @@
+Daily code to learn Java
